74d69afe90b95f30456a7994d234174ad88e15e7
fraromgon2us <fraromgon2@alum.us.es>
2021-02-10 17:37:44 +0100

    Despligue en Heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -160,11 +160,11 @@ STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
-
+APIS = {}
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
-
+BASEURL = 'https://examenfraromgon2.herokuapp.com'
 try:
     from local_settings import *
 except ImportError:
@@ -180,3 +180,5 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+import django_heroku
+django_heroku.settings(locals())
